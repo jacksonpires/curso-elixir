fd5c683c35019da732cf41f58d6e8d764a4e1929
Jackson Pires <jackson.pires@gmail.com>
2018-11-21 19:55:53 -0300

    Modulo 02 - Imutabilidade

---
+++ workspace/02/imutabilidade.exs
@@ -0,0 +1,15 @@
+total = 876
+
+defmodule Mutante do
+  def mutar(valor) do
+    valor = 1
+    IO.puts "interno- #{valor}" # Aqui será exibido 1 ou 876?
+    valor
+  end
+end
+
+Mutante.mutar(total)
+IO.puts "externo A- #{total}" # E aqui? 1 ou 876?
+
+total = Mutante.mutar(total)  # rebinding
+IO.puts "externo B- #{total}" # E agora, 1 ou 876?

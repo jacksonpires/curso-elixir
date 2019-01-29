6017f3a015f7d29b64d46cbf59f3bc40f469f171
Jackson Pires <jackson.pires@gmail.com>
2019-01-29 19:24:19 -0300

    Modulo 03 - Um pouco mais sobre o Mix

--- workspace/friends_app/lib/friends_app.ex
+++ workspace/friends_app/lib/friends_app.ex
@@ -1,18 +1,13 @@
 defmodule FriendsApp do
-  @moduledoc """
-  Documentation for FriendsApp.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FriendsApp.hello
-      :world
-
-  """
   def hello do
     :world
+  end
+  
+  def meu_ambiente do
+    case Mix.env() do
+      :prod -> "Ambiente de Produção"
+      :dev  -> "Ambiente de Desenvolvimento"
+      :test -> "Ambiente de Testes"
+    end
   end
 end

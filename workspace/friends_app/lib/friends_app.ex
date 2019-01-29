5ba20e7e17b24de50f192218a32b5edae19f4b05
Jackson Pires <jackson.pires@gmail.com>
2019-01-29 20:10:35 -0300

    Modulo 03 - Criando nossa primeira Task

--- workspace/friends_app/lib/friends_app.ex
+++ workspace/friends_app/lib/friends_app.ex
@@ -2,11 +2,11 @@ defmodule FriendsApp do
   def hello do
     :world
   end
-  
+
   def meu_ambiente do
     case Mix.env() do
       :prod -> "Ambiente de Produção"
-      :dev  -> "Ambiente de Desenvolvimento"
+      :dev -> "Ambiente de Desenvolvimento"
       :test -> "Ambiente de Testes"
     end
   end
---
+++ workspace/friends_app/lib/mix/tasks/start.ex
@@ -0,0 +1,8 @@
+defmodule Mix.Tasks.Start do
+  use Mix.Task
+
+  @shortdoc "Starts [Friends App]"
+  def run(_) do
+    IO.puts("\n\nIniciando a aplicação!!!\n\n")
+  end
+end

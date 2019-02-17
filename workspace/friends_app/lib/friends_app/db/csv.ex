c5428ed6a1beb69f566cd88eaa50158aeb10fa0b
Jackson Pires <jackson.pires@gmail.com>
2019-02-17 18:26:45 -0300

    Modulo 03 - Refatorando o create e o read para o delete

--- workspace/friends_app/lib/friends_app/db/csv.ex
+++ workspace/friends_app/lib/friends_app/db/csv.ex
@@ -16,22 +16,43 @@ defmodule FriendsApp.DB.CSV do
   end
   
   defp read do
+    get_struct_list_from_csv
+    |> show_friends
+  end
+  
+  defp get_struct_list_from_csv do
+    read_csv_file
+    |> parse_csv_file_to_list
+    |> csv_list_to_friend_struct_list
+  end
+  
+  defp read_csv_file do
     Application.fetch_env!(:friends_app, :csv_file_path)
     |> File.read!
+  end
+  
+  defp parse_csv_file_to_list(csv_file) do
+    csv_file
     |> CSVParser.parse_string(headers: false)
-    |> Enum.map( fn [email, name, phone] ->
+  end
+  
+  defp csv_list_to_friend_struct_list(list) do
+    list
+    |> Enum.map(fn [email, name, phone] ->
       %Friend{name: name, email: email, phone: phone}
     end)
+  end
+  
+  defp show_friends(friends_list) do
+    friends_list
     |> Scribe.console(data: [{"Nome", :name}, {"Email", :email}, {"Telefone", :phone}])
   end
   
   defp create do
     collect_data
-    |> Map.from_struct
-    |> Map.values
-    |> wrap_in_list
-    |> CSVParser.dump_to_iodata
-    |> save_csv_file
+    |> transform_on_wrapped_list
+    |> prepare_list_to_save_csv
+    |> save_csv_file([:append])
   end
 
   defp collect_data do
@@ -49,12 +70,24 @@ defmodule FriendsApp.DB.CSV do
     |> String.trim
   end
   
+  defp transform_on_wrapped_list(struct) do
+    struct
+    |> Map.from_struct
+    |> Map.values
+    |> wrap_in_list
+  end
+  
   defp wrap_in_list(list) do
     [list]
   end
   
-  defp save_csv_file(data) do
+  defp prepare_list_to_save_csv(list) do
+    list
+    |> CSVParser.dump_to_iodata
+  end
+  
+  defp save_csv_file(data, mode \\ []) do
     Application.fetch_env!(:friends_app, :csv_file_path)
-    |> File.write!(data, [:append])
+    |> File.write!(data, mode)
   end
 end
